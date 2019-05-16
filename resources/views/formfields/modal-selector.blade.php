6d4a27c04bd1e7eced6d69c680aee7dedb7ae95b
Olivier Mc Cann <olivier.mccann@ctrlweb.ca>
2019-05-16 10:11:03 -0400

    Made modal formfield more flexible

--- resources/views/formfields/modal-selector.blade.php
+++ resources/views/formfields/modal-selector.blade.php
@@ -3,11 +3,11 @@
 @endphp
 @if($modals->isNotEmpty())
 <select
-    class="form-control select2-ajax" name="modal"
+    class="form-control select2-ajax" name="{{ $row->field }}"
     data-get-items-route="/admin/modals/all"
 >
 @foreach($modals as $modal)
-    <option value="{{ $modal->id }}" @if(isset($dataTypeContent->modal) && $dataTypeContent->modal == $modal->id)selected="selected"@endif>{{ $modal->title }}</option>
+    <option value="{{ $modal->id }}" @if(isset($dataTypeContent->{$row->field}) && $dataTypeContent->{$row->field} == $modal->id)selected="selected"@endif>{{ $modal->title }}</option>
 @endforeach
 </select>
 @else
